1bbbb9b44111a3b57d8efd273fed0c5f9fa667e8
Jonathan DUPERRIER <jonathanduperrier@MacBook-Air-de-Jonathan.local>
2023-11-18 19:39:35 +0100

    get tracklist ok

--- src/components/spotifyComponent.tsx
+++ src/components/spotifyComponent.tsx
@@ -9,10 +9,7 @@ const SpotifyComponent: React.FC<SpotifyComponentProps> = ({ name }) => {
 
   const [tracks, setTracks] = useState<any[]>([]);
   const TOKEN_URL = 'https://accounts.spotify.com/api/token';
-  const AUTHORIZATION_URL = 'https://accounts.spotify.com/authorize';
-  const BASE_URL = 'https://api.spotify.com/v1/';
   const TRACKS_URL = 'https://api.spotify.com/v1/me/tracks';
-
   const CLIENT_ID = 'fade5aabb1904619864c4c7276102d48';
   const CLIENT_SECRET = 'be6740f27597409287902cdaa775434b';
   const APP_DEFAULT_URL = 'http://localhost:3000/';
@@ -25,7 +22,6 @@ const SpotifyComponent: React.FC<SpotifyComponentProps> = ({ name }) => {
   
     // Construct the Spotify authorization URL
     const authorizationUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${SCOPE}&response_type=code`;
-  
     // Redirect the user to the Spotify authorization URL
     window.location.href = authorizationUrl;
   };
@@ -38,32 +34,29 @@ const SpotifyComponent: React.FC<SpotifyComponentProps> = ({ name }) => {
 
   useEffect(() => {
     // Fetch data from Spotify API using the access token
-    const fetchData = async () => {  
-
+    const fetchData = async () => {
       const responseToken = await fetch(TOKEN_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
-      },
-      body: new URLSearchParams({
-        'grant_type': 'authorization_code',
-        'code': authorization_code,
-        'redirect_uri': 'http://localhost:3000/callback',
-      }),
-    });
-
-    const dataToken = await responseToken.json();
-    const accessToken = dataToken.access_token;
-
-
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+        },
+        body: new URLSearchParams({
+          'grant_type': 'authorization_code',
+          'code': authorization_code,
+          'redirect_uri': 'http://localhost:3000/callback',
+        }),
+      });
+      const dataToken = await responseToken.json();
+      const accessToken = dataToken.access_token;
+      
       const response = await fetch(TRACKS_URL, {
         headers: {
           'Authorization': `Bearer ` + accessToken,
         },
       });
-
       const data = await response.json();
+      console.log(data.items);
       setTracks(data.items);
     };
     fetchData();
@@ -72,9 +65,16 @@ const SpotifyComponent: React.FC<SpotifyComponentProps> = ({ name }) => {
   return (
     <div>
       <h1>Hello, {name}!</h1>
-      <h1>Display your Spotify profile data</h1>
-      
-
+      <h1>My Spotify Tracks</h1>
+      <ul>
+        {tracks ? (
+          tracks.map((track) => (
+            <li key={track.track.id}>{track.track.artists[0].name} - {track.track.name}</li>
+          ))
+        ) : (
+          <li>No tracks available</li>
+        )}
+      </ul>
     </div>
   );
 };
